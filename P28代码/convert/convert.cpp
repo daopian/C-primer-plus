53f26eb15cb641593adb99f26aa4f0b69408b54a
刀片批发 <84966110+daopian@users.noreply.github.com>
2022-07-13 10:30:24 +0800

    Add files via upload

---
+++ P28代码/convert/convert.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int stonetolb(int sts)
+{
+	return 14 * sts;
+}
+//写法问题前一张图片已经有过解释之后也不会再赘述
+int main()
+{
+	int stone;
+	cout << "Enter the weight in stone: ";
+	cin >> stone;
+	int pounds = stonetolb(stone);
+	cout << stone << " stone = " << pounds << " pounds.";
+	return 0;
+}
